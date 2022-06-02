9eaaa825d1af3458cbcecd872d460f1871643e10
wboetel <boetel.wolfdie-mb19@mb.dhbw-ravensburg.de>
2022-06-02 19:21:43 +0200

    gui.h

--- speicher.h
+++ speicher.h
@@ -17,24 +17,25 @@ char getChip(string coordinate){
     //Getter, der den Wert des Arrays field an der Stelle coordinate (zb. "A0") zu returnt.
     return field[int(coordinate[0])-65][coordinate[1]];
 }
-//BAUSTELLE WOLFI
+//------------------------BAUSTELLE WOLFI-------------------------------------(und Andireas)-----------
 int checkMill(string position){
-    int millsFound = 0;
     //Coordinates from A0 to C7
-
+    int millsFound = 0;
     //parsing coordinates from String to int
     int rotation (position[1]);
-    
     //Checking if stone is placed in corner
     if(rotation%2 == true || rotation == 0){
             //from start position checking in positive direction
             for(int i = rotation; i <= rotation + 2; i++){
                 //Comparing neigbour field to starting field
+                //Vielleicht geht das parsen hier schief???
                 if(getChip(string (position[0] + i)) == getChip(position){
-
+                //outta way, i'm wooorking here
                 }
-
-
+            }
+            //from start position checking in negative direction EXCEPTION for A0, B0, C0 required
+            for(int i = rotation; i > rotation - 2; i--){
+                //Work is in progress here
             }
     }else{
         //This is executed, when stone is not placed in corner
