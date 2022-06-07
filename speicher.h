e36af0c0db9d60d149a6950f66f347886c5c06c6
AndreasZi <andiziegl@web.de>
2022-06-07 11:43:31 +0200

    Mühlencheck finally done

--- speicher.h
+++ speicher.h
@@ -13,11 +13,26 @@ void setChip(string coordinate, char color)
     field[int(coordinate[0]) - 65][coordinate[1]] = color;
 }
 
+char getChip(char orbit, int rotation)
+{
+    // Getter, der den Wert des Arrays field an der Stelle coordinate (zb. "A0") zu returnt.
+    return field[int(orbit) - 65][rotation];
+}
+
+char getChip(char orbit, char rotation)
+{
+    // Getter, der den Wert des Arrays field an der Stelle coordinate (zb. "A0") zu returnt.
+    return field[int(orbit) - 65][rotation];
+}
+
 char getChip(string coordinate)
 {
     // Getter, der den Wert des Arrays field an der Stelle coordinate (zb. "A0") zu returnt.
     return field[int(coordinate[0]) - 65][coordinate[1]];
 }
+
+
+
 
 int checkMill(string position)
 {
@@ -26,33 +41,38 @@ int checkMill(string position)
 
     //  Checking if stone is placed in corner
     cout << "Modolo-Prüfung = " << int(position[1]) % 2 << endl;
+    cout << "getChip(position) = " << getChip(position) << endl;
+    cout << "getChip(position[0], int(position[1]) + 1) = " << getChip(position[0], int(position[1]) + 1) << endl;
+    cout << "getChip(position[0], int(position[1]) + 2) = " << getChip(position[0], int(position[1]) + 2) << endl;
+    //cout << "getChip(position[0], 0) = " << getChip(position[0], 0) << endl;
+    //cout << "position[1] = " << position[1] << endl;
 
     if (int(position[1]) % 2 == 0 || position[1] == '0')
     {
-        if (position[1] == '6' && getChip(position) == getChip(string({position[0], '7'})) == getChip(string({position[0], '0'})))
+        if (position[1] == '6' && getChip(position) == getChip(position[0], 7 + 48) && getChip(position) == getChip(position[0], 0 + 48))
         {
             millsFound++;
         }
-        else if (getChip(position) == getChip(string({position[0], position[1] + 1})) == getChip(string({position[0], position[1] + 2})))
+        else if (getChip(position) == getChip(position[0], int(position[1]) + 1) && getChip(position) == getChip(position[0], int(position[1]) + 2))
         {
             millsFound++;
         }
 
-        if (position[1] == '0' && getChip(position) == getChip(string({position[0], '6'})) == getChip(string({position[0], '7'})))
+        if (position[1] == '0' && getChip(position) == getChip(position[0], 6 + 48) && getChip(position) == getChip(position[0], 7 + 48))
         {
             millsFound++;
         }
-        else if (getChip(position) == getChip(string({position[0], position[1] - 1})) == getChip(string({position[0], position[1] - 2})))
+        else if (getChip(position) == getChip(position[0],  int(position[1]) - 1) && getChip(position) == getChip(position[0],  int(position[1]) - 2))
         {
             millsFound++;
         }
     }
-    else
+    /* else
     {
         // This is executed, when stone is not placed in corner
 
         // checking for radial mill
-        if (getChip(position) == getChip('A' + position(1)) == getChip('B' + position(1)) == getChip('C' + position(1)))
+        if (getChip(position) == getChip(string({'A' + position[1]})) == getChip(string({'B' + position[1]})) == getChip(string({'A' + position[1]})))
         {
             millsFound += 1;
         }
@@ -61,7 +81,7 @@ int checkMill(string position)
         {
             millsFound += 1;
         }
-    }
+    } */
     return millsFound;
 }
 void resetField()
