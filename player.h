3feea8ee18686849f9ecdfd1fb9aff53627d1a12
AndreasZi <andiziegl@web.de>
2022-06-10 13:43:27 +0200

    player.h, Bot::

--- player.h
+++ player.h
@@ -7,12 +7,43 @@ public:
     int unusedChips;
     int lostChips;
     char color;
-    Player(char COLOR)
+    Player(char);
+
+    void startTurn();
+    void loseChip();
+    char getColor();
+
+};
+
+
+Player::Player(char COLOR)
 {
     unusedChips = 9;
     lostChips = 0;
     color = COLOR;
 }
 
+void Player::startTurn(){
+
+}
+
+void Player::loseChip(){
+
+}
+
+class Bot: public Player
+{
+private:
+    /* data */
+public:
+    Bot(/* args */);
+    ~Bot();
 };
 
+Bot::Bot(/* args */)
+{
+}
+
+Bot::~Bot()
+{
+}
