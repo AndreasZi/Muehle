2e98c81e91b4742f9b62205ffc15f217c9d93b0c
wboetel <boetel.wolfdie-mb19@mb.dhbw-ravensburg.de>
2022-06-10 15:18:32 +0200

    Setup Arrays for Bot-Class

--- player.h
+++ player.h
@@ -1,10 +1,10 @@
-class Player //Erstelle eine Klasse namens Player
+class Player // Erstelle eine Klasse namens Player
 {
 private:
     /* data */
     string name;
-public:
 
+public:
     int unusedChips;
     int lostChips;
     char color;
@@ -16,16 +16,16 @@ public:
     string generateName();
     void setName(string);
     string getName();
-
 };
-
+#include <iostream>
+using namespace std;
 
 Player::Player(char COLOR)
 {
     unusedChips = 9;
     lostChips = 0;
     color = COLOR;
-    //name = generateName();
+    // name = generateName();
 }
 Player::Player(string NAME; char COLOR)
 {
@@ -35,25 +35,34 @@ Player::Player(string NAME; char COLOR)
     setName(NAME);
 }
 
-void Player::startTurn(){
-
+void Player::startTurn()
+{
 }
 
-void Player::loseChip(){
-
+void Player::loseChip()
+{
 }
 
-class Bot: public Player
+class Bot : public Player
 {
 private:
+    // char generateColor(); //Immer anders als Spieler
 
-    char generateColor(); //Immer anders als Spieler
+    // ARRAY der globalen Mühlen | 16 Mühlen a 3 Chips
+    string globalMills[16][3];
+    // ARRAY der Nachbarn | 24 ChipPlacements with max. 4 Neighbours
+    string neighbours[24][4];
+
+    void fillGMarray();
+    void fillNeigbourArray();
+
 public:
     Bot();
     //~Bot();
 
     void placeChip();
     void moveChip();
+    string getRemoveChipPosition();
 };
 
 Bot::Bot(char COLOR)
@@ -61,15 +70,42 @@ Bot::Bot(char COLOR)
     unusedChips = 9;
     lostChips = 0;
     color = COLOR;
+    name = 'Bot';
+    fillGMarray();
+    fillNeighbourArray();
 }
 
 Bot::Bot()
 {
     unusedChips = 9;
     lostChips = 0;
-    color = generateColor();
+    // color = generateColor();
+    name = 'Bot';
+    fillGMarray();
+    fillNeighbourArray();
 }
 
 /*Bot::~Bot(){
 
 }*/
+
+// AB HIER KOMMT ALLES IN Bot.cpp---------------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+fillGMarray()
+{
+}
+fillNeighbourArray()
+{
+}
+
+void placeChip()
+{
+}
+void moveChip()
+{
+}
+
+// HIER ENDET Bot.cpp-----------------------------------------------------------------------------------------------------
