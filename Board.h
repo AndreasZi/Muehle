add4283d185f4dd9aa970276741d84af35af5ebd
AndreasZi <andiziegl@web.de>
2022-07-19 19:01:15 +0200

    Kommentasre in der Board.h

--- Board.h
+++ Board.h
@@ -20,16 +20,31 @@ public:
     string Neighbours[4];
     
     void setChip(char, char, Player&);
-    void deleteChip(char, char);
+    //Platziere einen Chip auf einem Feld des Spielfelds
+    
     char getChip(char, char);
+    //Gibt die Farbe des Spielers zurück, dem der Chip an einer gewissen Position gehört
+
     Player* getPlayer(char, char);
+    //Gibt den Spieler zurück, dem der Chip an einer gewissen Position gehört
 
     char getEmptyColor();
 
     void emptyBoard();
+    //Funktion zum Bereinigen des gesamten Spielfelds
+    
+    void deleteChip(char, char);
+    //Funktion zum Bereinigen eines einzelnen Felds
+
     void printBoard();
+    //Funktion zur graphischen Darstellung des Spielfelds
+
     int checkMill(char, char);
+    //Prüft ob ein Feld Teil einer Mühle ist
+
     bool checkNeighbour(char, char, char, char);
+    //Prüft ob zwei Felder benachbart sind
+
     tuple<string, string, string, string> getNeighbours(string);
     void printNeighbours(string);
 
